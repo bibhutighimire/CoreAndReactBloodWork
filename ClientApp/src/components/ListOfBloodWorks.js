85f45631f5ee050a5060ef15aa682a695b082e3b
bibhutighimire <ghimirebibhuti@gmail.com>
2021-01-11 13:04:45 -0700

    added listofbloodwork and working on displaying data in UI

---
+++ ClientApp/src/components/ListOfBloodWorks.js
@@ -0,0 +1,78 @@
+﻿import React, { Component } from 'react';
+import axios from 'axios';
+
+export class ListOfBloodWorks extends Component {
+    static displayName = ListOfBloodWorks.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { bloodworks: [], loading: true };
+    }
+
+    componentDidMount() {
+        // 2) When the component mounts, we make the async call to the server to retrieve the API results.
+        this.populateBloodworksData();
+    }
+
+
+    static renderBloodWorkTable(bloodworks) {
+        return (
+            <table className='table table-striped' aria-labelledby="tabelLabel">
+                <thead>
+                    <tr>
+                        <th>Date Created</th>
+                        <th>Exam Date</th>
+                        <th>Results Date</th>
+                        <th>Description</th>
+                        <th>Hemoglobin</th>
+                        <th>Hematocrit</th>
+                        <th>White Blood Cell Count (Number)</th>
+                        <th>Red Blood Cell Count (Number)</th>
+                      
+
+                    </tr>
+                </thead>
+                <tbody>
+                    {bloodworks.map(bloodworks =>
+                        <tr id={bloodworks.BloodWorkID}>
+                            <td>{bloodworks.DateCreated}</td>
+                            <td>{bloodworks.ExamDate}</td>
+                            <td>{bloodworks.ResultsDate}</td>
+                            <td>{bloodworks.Description}</td>
+                            <td>{bloodworks.Hemoglobin}</td>
+                            <td>{bloodworks.Hematocrit}</td>
+                            <td>{bloodworks.WhiteBloodCellCount}</td>
+                            <td>{bloodworks.RedBloodCellCount}</td>
+
+
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : ListOfBloodWorks.renderBloodWorkTable(this.state.bloodworks);
+
+        return (
+            <div>
+                <h1 id="tabelLabel" >List of all the blood works</h1>
+                <p>List of all the blood works are displayed below:</p>
+                {contents}
+            </div>
+        );
+    }
+
+
+    async populateBloodworksData() {
+        axios.get(`api/bloodworks/All`)
+            .then(res => {
+                const data = res.data;
+                this.setState({ bloodworks: data, loading: false });
+            })
+    }
+}
---
+++ Controllers/BloodWorksAPIController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CoreAndReact.Models;
+
+namespace CoreAndReact.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BloodWorksAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BloodWorksAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //https://localhost:44307/api/BloodWorksAPI
+        // GET: api/BloodWorksAPI
+        [HttpGet("bloodworks/All")]
+        public async Task<ActionResult<IEnumerable<BloodWork>>> GetBloodWork()
+        {
+            
+           
+            return await _context.BloodWork.ToListAsync();
+        }
+
+        // GET: api/BloodWorksAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BloodWork>> GetBloodWork(int id)
+        {
+            var bloodWork = await _context.BloodWork.FindAsync(id);
+
+            if (bloodWork == null)
+            {
+                return NotFound();
+            }
+
+            return bloodWork;
+        }
+
+        // PUT: api/BloodWorksAPI/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBloodWork(int id, BloodWork bloodWork)
+        {
+            if (id != bloodWork.BloodWorkID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(bloodWork).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BloodWorkExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/BloodWorksAPI
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<BloodWork>> PostBloodWork(BloodWork bloodWork)
+        {
+            _context.BloodWork.Add(bloodWork);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBloodWork", new { id = bloodWork.BloodWorkID }, bloodWork);
+        }
+
+        // DELETE: api/BloodWorksAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBloodWork(int id)
+        {
+            var bloodWork = await _context.BloodWork.FindAsync(id);
+            if (bloodWork == null)
+            {
+                return NotFound();
+            }
+
+            _context.BloodWork.Remove(bloodWork);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BloodWorkExists(int id)
+        {
+            return _context.BloodWork.Any(e => e.BloodWorkID == id);
+        }
+    }
+}
