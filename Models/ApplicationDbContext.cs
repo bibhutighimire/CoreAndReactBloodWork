584b31d8ba02f76985bab9a58af08e7f32a833c2
bibhutighimire <ghimirebibhuti@gmail.com>
2021-01-10 21:36:45 -0700

    react added to project. model created. database connected

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndReact.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public DbSet<BloodWork> BloodWork { get; set; }
+
+        public ApplicationDbContext()
+           : base()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions options)
+           : base(options)
+        {
+        }
+    }
+}
---
+++ Models/BloodWork.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreAndReact.Models
+{
+    public class BloodWork
+    {
+        public int BloodWorkID { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public string DateCreated { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public string ExamDate { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public string ResultDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(max)")]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        [RegularExpression(@"^[0-9.-]*$", ErrorMessage = "Please enter numbers only.")]
+        public string Hemoglobin { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        [RegularExpression(@"^[0-9.-]*$", ErrorMessage = "Please enter numbers only.")]
+        public string Hematocrit { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        [RegularExpression(@"^[0-9.-]*$", ErrorMessage = "Please enter numbers only.")]
+        public string WhiteBloodCellCount { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(10)")]
+        [RegularExpression(@"^[0-9.-]*$", ErrorMessage = "Please enter numbers only.")]
+        public string RedBloodCellCount { get; set; }
+    }
+}
